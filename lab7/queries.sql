3898587f6e9bdadda707f70bb3c4ddc1fe51729d
james ooi <65988711+galacticgoob@users.noreply.github.com>
2021-05-23 23:35:52 -0700

    Update queries.sql

--- lab7/queries.sql
+++ lab7/queries.sql
@@ -5,8 +5,8 @@ SELECT Count(*) FROM(
 		FROM part_nyc NY, color C1
 		WHERE NY.color = C1.color_id AND C1.color_name = 'Red' UNION ALL
 	SELECT part_number
-		FROM part_sfo SF, C2.color
-		WHERE SF.color = C2.color_id AND color_name = 'Red');
+		FROM part_sfo SF, color C2
+		WHERE SF.color = C2.color_id AND color_name = 'Red') AS PartCount;
 
 SELECT S.supplier_name FROM supplier S
 	INNER JOIN part_nyc NY ON S.supplier_id = NY.supplier
@@ -20,6 +20,6 @@ SELECT supplier_name
 	AND supplier_id 
 	NOT IN (SELECT supplier FROM part_sfo);
 
-UPDATE part_nyc SET on_hand = 10;
+UPDATE part_nyc SET on_hand = -10;
 
-DELETE* FROM part_nyc WHERE on_hand < 30;
+DELETE FROM part_nyc WHERE on_hand < 30;
