59bfb3a82dd8f22a7966b6f274b81e9f2091b0e9
james ooi <65988711+galacticgoob@users.noreply.github.com>
2021-05-25 21:48:24 -0700

    Update queries.sql

--- lab7/queries.sql
+++ lab7/queries.sql
@@ -1,17 +1,23 @@
-SELECT Count(part_number) AS PartCount FROM part_nyc WHERE on_hand > 70;
+SELECT Count(part_number) AS PartCount
+	FROM part_nyc
+	WHERE on_hand > 70;
 
-SELECT Count(*) FROM(
-	SELECT part_number
-		FROM part_nyc NY, color C1
-		WHERE NY.color = C1.color_id AND C1.color_name = 'Red' UNION ALL
-	SELECT part_number
-		FROM part_sfo SF, color C2
-		WHERE SF.color = C2.color_id AND color_name = 'Red') AS PartCount;
+SELECT Count(*) As PartCount
+	FROM(
+		SELECT part_number
+			FROM part_nyc NY, color C1
+			WHERE NY.color = C1.color_id AND C1.color_name = 'Red' UNION ALL
+		SELECT part_number
+			FROM part_sfo SF, color C2
+			WHERE SF.color = C2.color_id AND color_name = 'Red');
 
-SELECT S.supplier_name FROM supplier S
+SELECT S.supplier_name
+	FROM supplier S
 	INNER JOIN part_nyc NY ON S.supplier_id = NY.supplier
 	INNER JOIN part_sfo SF ON S.supplier_id = SF.supplier
+	GROUP BY S.supplier_name
 	HAVING SUM(NY.on_hand) > SUM(SF.on_hand);
+
 
 SELECT supplier_name
 	FROM supplier 
